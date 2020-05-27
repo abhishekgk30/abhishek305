36e2b34c2dd9ea24a243e9c2c5aa817240f9b023
abhishekgk30 <65482628+abhishekgk30@users.noreply.github.com>
2020-05-27 14:54:49 +0530

    dice game

---
+++ index.js
@@ -0,0 +1,34 @@
+
+// first dice
+
+var randomNumber1= Math.floor(Math.random() * 6) +1;
+
+var randomImageSource="images/dice" + randomNumber1 + ".png";
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+// second dice
+
+var randomNumber2= Math.floor(Math.random() * 6) +1;
+
+var randomImageSource2= "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").innerHTML =" Player 1 Wins!!";
+
+}
+
+else if(randomNumber1 < randomNumber2 )
+{
+  document.querySelector("h1").innerHTML =" Player 2 Wins!!";
+}
+
+else
+{
+  document.querySelector("h1").innerHTML =" Draww!!";
+}
